3cea91ac64a089bf4bac428ccf9bf649abada0e3
나눔 <sksyag@naver.com>
2021-03-21 21:47:52 +0900

    Exception 처리. 404 外 페이지는 500으로 처리

---
+++ HealthShareSecurity/src/main/java/com/cafe24/healthshare/exception/ExceptionAdvice.java
@@ -0,0 +1,30 @@
+package com.cafe24.healthshare.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import lombok.extern.slf4j.Slf4j;
+
+@ControllerAdvice
+@Slf4j
+public class ExceptionAdvice {
+	
+	@ExceptionHandler(Exception.class)
+	public String Exception(Exception e, Model m){
+		log.error("................Exception..............."+e.getMessage());
+		m.addAttribute("exception",e);
+		log.error("................Model " +m);		
+		return "inc/error500";
+	}
+	
+	@ExceptionHandler(NoHandlerFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public String Exception404(NoHandlerFoundException e) {
+		return "inc/error404";
+	}
+	
+}
